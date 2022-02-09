b635bca091ad368405a9e23052fdcf04a439f63e
Vladimir Derkachev <soprascey@gmail.com>
2022-02-09 13:40:19 +0300

    NewNoteActivity

---
+++ app/src/main/java/com/example/shoppinglist/db/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglist.db
+
+import androidx.lifecycle.*
+import com.example.shoppinglist.entities.NoteItem
+import kotlinx.coroutines.launch
+
+class MainViewModel(dataBase: MainDataBase): ViewModel() {
+    val dao = dataBase.getDao()
+    val allNotes: LiveData<List<NoteItem>> = dao.getAllNotes().asLiveData()
+    fun insertNote(note: NoteItem) = viewModelScope.launch {
+        dao.insertNote(note)
+    }
+
+    class  MainViewModelFactory(val database: MainDataBase) : ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+                @Suppress("UNCHECKED_CAST")
+                return  MainViewModel(database) as T
+            }
+            throw IllegalAccessException("Unknown ViewModelClass")
+        }
+
+    }
+}
