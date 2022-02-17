79eb19029a9da05d9f73b032a7edf96e55f85169
Vladimir <soprascey@gmail.com>
2022-02-17 12:06:01 +0300

    Interstitial ads

--- app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
@@ -13,12 +13,21 @@ import com.example.shoppinglist.fragments.FragmentManager
 import com.example.shoppinglist.fragments.NoteFragment
 import com.example.shoppinglist.fragments.ShopListNamesFragment
 import com.example.shoppinglist.settings.SettingsActivity
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     lateinit var binding: ActivityMainBinding
     private lateinit var defPref: SharedPreferences
     private var currentMenuItemId = R.id.share_list
     private var currentTheme = ""
+    private var iAd: InterstitialAd? = null
+    private var adShowCounter = 0
+    private var adShowCounterMax = 3
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,17 +40,71 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
         setContentView(binding.root)
         FragmentManager.setFragment(ShopListNamesFragment.newInstance(),this)
         setBottomNavListener()
+        loadInterAd()
+    }
+
+    private fun loadInterAd(){
+        val request = AdRequest.Builder().build()
+        InterstitialAd.load(this, getString(R.string.inter_ad_id), request,
+            object : InterstitialAdLoadCallback(){
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    iAd = ad
+                }
+
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    iAd = null
+                }
+            })
+    }
+
+    private fun showInterAd(adListener: AdListener){
+        if(iAd != null && adShowCounter > adShowCounterMax ){
+            iAd?.fullScreenContentCallback = object : FullScreenContentCallback(){
+                override fun onAdDismissedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+                    adListener.onFinish()
+
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    iAd = null
+                    loadInterAd()
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+
+                }
+            }
+            adShowCounter = 0
+           iAd?.show(this)
+        } else {
+            adShowCounter++
+            adListener.onFinish()
+        }
     }
 
     private fun setBottomNavListener(){
         binding.bNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.settings->{
-                    startActivity(Intent(this, SettingsActivity::class.java))
+                    showInterAd(object :AdListener{
+                        override fun onFinish() {
+                            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+                        }
+                    })
+
                 }
                 R.id.notes->{
-                    currentMenuItemId = R.id.notes
-                    FragmentManager.setFragment(NoteFragment.newInstance(),this)
+                    showInterAd(object :AdListener{
+                        override fun onFinish() {
+                            currentMenuItemId = R.id.notes
+                            FragmentManager.setFragment(NoteFragment.newInstance(),this@MainActivity)
+                        }
+                    })
+
                 }
                 R.id.shop_list->{
                     currentMenuItemId = R.id.share_list
@@ -76,5 +139,9 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
 
     override fun onClick(name: String) {
         Log.d("MyLog", "Name: $name")
+    }
+
+    interface AdListener{
+        fun onFinish()
     }
 }
